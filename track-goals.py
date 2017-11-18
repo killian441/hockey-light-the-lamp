bceb9d0d67a1fe12e6b371444d97d92c764cb600
Mike Killian <killian441@gmail.com>
2017-11-18 15:42:23 -0700

    added open_game_url

--- track-goals.py
+++ track-goals.py
@@ -56,8 +56,8 @@ PWD = os.path.join(os.path.dirname(__file__))
 
 def get_game_url(gamenumber=None, team="avalanche"):
     '''Returns a string for the URL pointing to the game. Given a gamenumber 
-     (i.e. 2017020285) no other parameter is needed. Lacking a gamenumber, then
-     a team is required to find the scheduled game for today.'''
+       (i.e. 2017020285) no other parameter is needed. Lacking a gamenumber,
+       then a team is required to find the scheduled game for today.'''
     if gamenumber:
         return BASE_URL.format(gamenumber)
 
@@ -80,5 +80,26 @@ def get_game_url(gamenumber=None, team="avalanche"):
     else:
         raise Exception("Cannot find game today for specified team")
 
+def open_game_url(url=None):
+    '''Open url, verify that the game is live and then return the current
+       gamenumber, timestamp, and score.'''
+    if not url:
+        url = get_game_url()
+
+    resp = urlopen(url)
+    raw_data = resp.read()
+    data = json.loads(raw_data)
+
+    if data['gameData']['status']['abstractGameState'] != 'Live':
+        raise Exception('Game is not live')
+
+    gamenumber = data['gamePk']
+    timestamp = data['metaData']['timeStamp']
+
+    goals = data['liveData']['plays']['currentPlay']['about']['goals']
+
+    return(gamenumber, timestamp, goals)
+
+    
 
 
